c31ccb79db028a0cf8fe32f787ea86925e76a231
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-06-18 19:00:36 +0700

    add plan details

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,29 +10,34 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: navycolor,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 53),
-              child: Image.asset('assets/images/goal.png'),
-            ),
-            RichText(
-              text: TextSpan(text: 'Upgrade to ', style: title, children: [
-                TextSpan(text: 'Pro', style: title.copyWith(color: bluecolor)),
-              ]),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              'Go unlock all features and\nbuild your own bussines bigger',
-              style: body,
-              textAlign: TextAlign.center,
-            ),
-            PlanList()
-          ],
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 50, horizontal: 53),
+                child: Image.asset('assets/images/goal.png'),
+              ),
+              RichText(
+                text: TextSpan(text: 'Upgrade to ', style: title, children: [
+                  TextSpan(
+                      text: 'Pro', style: title.copyWith(color: bluecolor)),
+                ]),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Go unlock all features and\nbuild your own bussines bigger',
+                style: body,
+                textAlign: TextAlign.center,
+              ),
+              PlanList()
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/plan_list.dart
+++ lib/pages/plan_list.dart
@@ -19,21 +19,21 @@ class _PlanListState extends State<PlanList> {
       padding: EdgeInsets.symmetric(horizontal: 32, vertical: 50),
       child: Column(
         children: [
-          planCard(0),
+          planCard(0, 'Weekly', 'Good for learn', '\$5'),
           SizedBox(
             height: 10,
           ),
-          planCard(1),
+          planCard(1, 'Mountly', 'Good for starting up', '\$5'),
           SizedBox(
             height: 10,
           ),
-          planCard(2),
+          planCard(2, 'Yearly', 'Steady company', '\$5'),
         ],
       ),
     );
   }
 
-  Widget planCard(int index) {
+  Widget planCard(int index, String titles, String subtitle, String price) {
     return GestureDetector(
       onTap: () {
         setState(() {
@@ -56,6 +56,18 @@ class _PlanListState extends State<PlanList> {
                   'assets/images/check.png',
                   width: 18,
                 ),
+          title: Text(
+            titles,
+            style: bodytitle,
+          ),
+          subtitle: Text(
+            subtitle,
+            style: body.copyWith(color: whitecolor, fontSize: 12),
+          ),
+          trailing: Text(
+            price,
+            style: title.copyWith(fontSize: 18),
+          ),
         ),
       ),
     );
