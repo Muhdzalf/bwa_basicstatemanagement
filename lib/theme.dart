5bbbaeb5ffd8f069f85514c1cec15324a778382d
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-06-18 17:09:13 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color
+Color navycolor = Color(0xff04112F);
+Color bluecolor = Color(0xff007DFF);
+Color textcolor = Color(0xff4D5B7C);
+Color whitecolor = Colors.white;
+
+// TextStyle
+TextStyle title = GoogleFonts.poppins(
+    color: whitecolor, fontWeight: FontWeight.w700, fontSize: 26);
+TextStyle body = GoogleFonts.poppins(
+    color: textcolor, fontWeight: FontWeight.w300, fontSize: 16);
+TextStyle bodytitle = GoogleFonts.poppins(
+    color: whitecolor, fontWeight: FontWeight.w500, fontSize: 14);
